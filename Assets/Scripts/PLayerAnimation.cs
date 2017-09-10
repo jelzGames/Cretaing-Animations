7c60b9799592f6d2b5e32cf400466e205e8580ce
JESUS LOPEZ <jelz2yk@JESUSs-MacBook-Pro.local>
2017-09-10 11:57:51 +0300

    animations

---
+++ Assets/Scripts/PLayerAnimation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerAnimation : MonoBehaviour {
+
+    private Animator anim;
+    private string SLEEP_END_ANIMATION = "sleep_end";
+    private string IDLE_ANIMATION = "idle";
+	private string WALK_PARAMETER = "Walk";
+    private string ATTACK_1_PARAMETER = "Attack1";
+    private string ATTACK_2_PARAMETER = "Attack2";
+
+
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Attack1()
+	{
+        anim.SetBool(ATTACK_1_PARAMETER, true);
+	}
+
+	public void EndAttack1()
+	{
+		anim.SetBool(ATTACK_1_PARAMETER, false);
+	}
+
+	public void Attack2()
+	{
+		anim.SetBool(ATTACK_2_PARAMETER, true);
+	}
+
+	public void EndAttack2()
+	{
+		anim.SetBool(ATTACK_2_PARAMETER, false);
+	}
+
+
+	public void PlayerWalk(bool walk)
+	{
+        anim.SetBool(WALK_PARAMETER,walk);
+	}
+
+
+	void Endsleep()
+	{
+        anim.Play(SLEEP_END_ANIMATION);
+	}
+
+	void BeginIdle()
+	{
+		anim.Play(IDLE_ANIMATION);
+	}
+}
---
+++ Assets/Scripts/PlayerWalk.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour {
+
+    public float rotationSpeed = 3f;
+    private float rotateY;
+    public float h, v;
+
+    PLayerAnimation playerAnimation;
+
+	// Use this for initialization
+	void Awake () {
+        playerAnimation = GetComponent<PLayerAnimation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckMovement();
+        AnimatePlauer();
+        CheckForAttack();
+
+	}
+
+	void CheckForAttack()
+	{
+        if (Input.GetMouseButtonDown(0))
+		{
+            playerAnimation.Attack1();
+		}
+		if (Input.GetMouseButtonDown(1))
+		{
+            playerAnimation.Attack2();
+		}
+
+	}
+
+	void CheckMovement()
+	{
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        rotateY -= h * rotationSpeed;
+
+        transform.localRotation = Quaternion.AngleAxis(rotateY, Vector3.up);
+	}
+
+	void AnimatePlauer()
+	{
+        if (v != 0 )
+		{
+            playerAnimation.PlayerWalk(true);
+
+	    }
+		else
+		{
+			playerAnimation.PlayerWalk(false);
+
+		}
+	}
+}
